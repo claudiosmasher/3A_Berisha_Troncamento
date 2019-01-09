21750b56f70e42b67d50bcffa2645322fe1898af
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:03:27 +0100

    Add project files.

---
+++ ConsoleTroncamento1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTroncamento1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inizializzazione variabili
+            double ore = 100;
+            double errore = 0.000000095;
+            double velocità = 1676;
+
+
+            
+
+        }
+    }
+}
