f12512900975c4a137050507217b20d7addf9be7
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:20:25 +0100

    Scrittura di codice con variabile velocità non in input

--- ConsoleTroncamento1/Program.cs
+++ ConsoleTroncamento1/Program.cs
@@ -11,13 +11,21 @@ namespace ConsoleTroncamento1
         static void Main(string[] args)
         {
             // Inizializzazione variabili
-            double ore = 100;
-            double errore = 0.000000095;
+         
+            double erroretroncamento = 0.000000095;
             double velocità = 1676;
+            double erroretempo = 0;
+            double errorespazio = 0;
 
+            erroretempo = erroretroncamento * 100 * 60 * 60 * 10;
 
-            
+            errorespazio = velocità * erroretempo;
 
+            Console.WriteLine($"Il tempo calcolato in modo errato è {erroretempo}");
+            Console.WriteLine(" ");
+            Console.WriteLine($"Lo spazio calcolato in modo errato è {errorespazio}");
+
+            Console.ReadLine();
         }
     }
 }
